23aa5ac7d08e5f89bf2818f6beebdac464233eae
capitancambio <capitan.cambio@gmail.com>
2013-08-05 16:54:53 +0100

    First commit

---
+++ subcommand.go
@@ -0,0 +1,276 @@
+//Package subcommand is an option parsing utility a la git/mercurial/go loosely 
+//inspired by Ruby's OptionParser 
+package subcommand
+import(
+	"strings"
+	"errors"
+	"fmt"
+)
+
+//parser:=new(Parser).AddFlag("--size ","-s","size of what ever")
+//parser.AddFlag("--cool" , "-c" ,"size", func (value String));
+//parser.AddCommand("cosa").AddFlag("--cosa","-c"," cosa ");
+//struct parser
+//struct command
+//struct flag
+
+//FlagType defines the different flag types. Options have values associated to the flag, Switches have no value associated.
+type FlagType int
+const(
+	Option FlagType = iota
+	Switch
+)
+
+//Command aggregates different flags under a common name. Every time a command is found during the parsing process the associated function is executed.
+type Command struct{
+	//Name 
+	Name string
+	innerFlagsLong map[string] *Flag
+	innerFlagsShort map[string] *Flag
+	fn func(string)
+}
+//Parser is a command itself and contains other commands. It's the 
+//data structure and its name should be the executable name.
+type Parser struct{
+	Command
+	Commands map[string] *Command
+
+}
+//NewParser constructs a parser for program name given
+func NewParser(program string) *Parser{
+	parser:=new(Parser)
+	parser.innerFlagsShort=make(map[string] *Flag)
+	parser.innerFlagsLong=make(map[string] *Flag)
+	parser.Commands=make(map[string] *Command)
+	parser.Name=program
+	return parser
+}
+
+//AddCommand inserts a new subcommand to the parser
+func (parser *Parser) AddCommand(name string,fn func(string)) (command *Command,err error){
+	if _,exists:=parser.Commands[name];exists{
+		return nil,errors.New(fmt.Sprintf("Command '%s' already exists ",command))
+	}
+	//create the command
+	command=new(Command)
+	command.Name=name
+	command.innerFlagsShort=make(map[string] *Flag)
+	command.innerFlagsLong=make(map[string] *Flag)
+	command.fn=fn
+	//add it to the parser
+	parser.Commands[name]=command
+	return command,nil
+}
+
+
+//AddFlag adds a new switch or option to the command. 
+//The distinction between switches and options is made from the long definition:
+//
+// "--option OPTION" expects a value after the flag 
+// "--switch " does not expect a value
+//
+//The short definition has no length restriction but it should be significantly shorter that its long counterpart  
+//Example:
+//command.AddFlag("--thing THING","-t",thingProcessor)//option
+//command.AddFlag("--tacata","-ta",isTacata)//switch
+func (command *Command) AddFlag(long string,short string,description string, fn func(string))(flag *Flag,err error){
+	flag,err=buildFlag(long,short,description,fn)
+	if (err!=nil){
+		return nil,err
+	}
+	if _,exists:=command.innerFlagsLong[flag.Long];exists{
+		return nil,errors.New(fmt.Sprintf("Flag '%s' already exists ",long))
+	}
+	command.innerFlagsLong[flag.Long]=flag;
+
+	if _,exists:=command.innerFlagsShort[flag.Short];exists{
+		return nil,errors.New(fmt.Sprintf("Flag '%s' already exists ",long))
+	}
+	command.innerFlagsShort[flag.Short]=flag;
+	return flag,nil
+
+}
+
+//flagged is convenience interface for treating commands and parsers equally
+type flagged interface{
+	getShortFlag(name string) (flag *Flag,ok bool)
+	getLongFlag(name string) (flag *Flag,ok bool)
+	getFlags() []Flag
+	getName() string
+}
+
+//getShortFlag returns the flag for the given short definition or false if it's not present
+func (command *Command) getShortFlag(name string) (flag *Flag,ok bool){
+	flag,ok=command.innerFlagsShort[name]
+	return
+}
+
+//getLongFlag returns the flag for the long definition or false if it's not present
+func (command *Command) getLongFlag(name string) (flag *Flag,ok bool){
+	flag,ok=command.innerFlagsLong[name]
+	return
+}
+//getName returns the command name
+func (command *Command) getName() string{
+	return command.Name
+}
+
+//getFlags returns a slice containing the command's flags  
+func (command *Command) getFlags() []Flag{
+	//return command.Name
+	flags:=make([]Flag,0)
+	for _,val:=range command.innerFlagsLong{
+		flags=append(flags,*val)
+	}
+	return flags
+}
+
+
+//Parse parses the arguments executing the associated functions for each command and flag. It returns the left overs if some non-option strings were not processed. Errors are returned in case an unknown flag is found or a mandatory flag was not supplied.
+func (parser *Parser) Parse( args[]string) (leftOvers []string,err error){
+	leftOvers=make([]string,0)
+	visited:=make([]Flag,0)
+	var currentCommand flagged=parser
+	//go comsuming options commands and sub-options
+	for i:=0; i<len(args); i++{
+		arg:=args[i]
+		if strings.HasPrefix(arg,"-") { //flag
+			var opt *Flag
+			var ok bool
+			if strings.HasPrefix(arg,"--"){
+				opt,ok=currentCommand.getLongFlag(arg)
+			}else{
+				opt,ok=currentCommand.getShortFlag(arg)
+			}
+			//not present
+			if !ok {
+				err=errors.New(fmt.Sprintf("%v is not a valid flag for %v",arg,currentCommand.getName()))
+				//show help?
+				return
+			}
+			if opt.Type==Option {//option
+				if i+1>=len(args){
+					err=errors.New(fmt.Sprintf("No value for option %v",arg))
+					return
+				}
+				i++
+				opt.fn(args[i])
+			}else{//switch
+				opt.fn("")
+			}
+			//add to visited options
+			visited=append(visited,*opt)
+		}else{
+			//_,isParser:=currentCommand.(Parser)
+			if ok,flag:=checkVisited(visited,currentCommand.getFlags()); !ok{
+				err=errors.New(fmt.Sprintf("%v was not found and is mandatory for %v",flag,currentCommand))
+				return
+			}
+			cmd,ok:=parser.Commands[arg]
+			if ok {
+				currentCommand=cmd
+				cmd.fn(arg)
+			}else{
+				leftOvers=append(leftOvers,arg)
+			}
+
+		}
+
+	}
+
+	return leftOvers,nil
+}
+//checks if the mandatory flags were visited
+func checkVisited(visited []Flag,commandFlags []Flag) (visted bool, err Flag){
+	for _,flag := range commandFlags{
+		if flag.Mandatory{
+			ok:=false
+			for _,vFlag:=range visited{
+				if vFlag.Long==flag.Long{
+					ok=true
+					break
+				}
+			}
+			if !ok{
+				return false,flag
+			}
+		}
+	}
+	return true,err
+}
+
+//Flag structure
+type Flag struct{
+	//long definition (--option OPTION)
+	Long string
+	//Short definition (-o )
+	Short string
+	//Description 
+	Description string
+	//FlagType, option or switch
+	Type FlagType
+	//Function to call when the flag is found during the parsing process
+	fn func(string)
+	//Says if the flag is optional or mandatory
+	Mandatory bool
+}
+
+//Must sets the flag as mandatory. The parser will raise an error in case it isn't present in the arguments
+func (flag Flag) Must(isIt bool){
+	flag.Mandatory=isIt
+}
+
+//parse flag type
+func getFlagType(flag string) (fType FlagType, err error){
+	//is a switch?
+	parts:=strings.Split(flag," ")
+	l:=len(parts)
+	switch {
+		case len(parts[0])==0:
+			return -1, errors.New(fmt.Sprintf("Flag is empty"))
+		case l==1: return Switch,err
+		case l==2: return Option,err
+	default:
+			return -1, errors.New(fmt.Sprintf("Flag '%s' has more than 2  words",flag))
+	}
+}
+
+//parse long definition
+func getFlagLonfDefinition(flag string) (name string,err error){
+	name=strings.Split(flag," ")[0]
+	if (!strings.HasPrefix(name,"--")){
+		return "",errors.New(fmt.Sprintf("Flag '%s' has to start by --",flag))
+	}
+	return name,nil
+}
+
+//parse short definition
+func getFlagShortDefinition(flag string) (name string,err error){
+	parts:=strings.Split(flag," ")
+	if(len(parts)>1){
+		return "",errors.New(fmt.Sprintf("Short flag must have only one word",flag))
+	}else if (!strings.HasPrefix(parts[0],"-")){
+		return "",errors.New(fmt.Sprintf("Flag '%s' has to start by -",flag))
+	}
+	return parts[0],nil
+}
+
+
+//builds the flag struct
+func buildFlag(long string, short string, desc string, fn func(string) ) (flag *Flag,err error){
+	flag=new(Flag)
+	flag.Type,err=getFlagType(long)
+	if(err!=nil){
+		return nil,err
+	}
+
+	flag.Long,err=getFlagLonfDefinition(long)
+	if(err!=nil){
+		return nil,err
+	}
+	flag.Short,err=getFlagShortDefinition(short)
+	flag.fn=fn
+	flag.Mandatory=false
+	return flag,nil
+}
+
---
+++ subcommand_test.go
@@ -0,0 +1,235 @@
+package subcommand
+
+import(
+	"testing"
+)
+
+var assert string = "expected %v but was %v";
+var emptyFn =func (value string){};
+func assertEquals(t *testing.T,exp string, res string ) {
+	if(exp!=res){
+		t.Error(assert,exp,res)
+	}
+}
+func assertEqualsI(t *testing.T,exp int,res int) {
+	if(exp!=res){
+		t.Error(assert,exp,res)
+	}
+
+}
+func assertNil( t *testing.T,err error){
+	if(err!=nil){
+		t.Error("Error is not nil")
+	}
+}
+
+//build option full 
+func TestParserFlagOptionFull(t *testing.T) {
+	parser:=NewParser("test")
+	option,err:=parser.AddFlag("--option OPT","-o","This is an option",emptyFn)
+
+	assertNil(t,err);
+	if option==nil { t.Error("option is nil")}
+	if _,exists:=parser.innerFlagsLong[option.Long];!exists{
+		t.Error("option is not present in the long names")
+	}
+
+	if _,exists:=parser.innerFlagsShort[option.Short];!exists{
+		t.Error("option is not present in the short names")
+	}
+}
+
+
+
+func TestBuildFlag(t *testing.T){
+	flag,err:=buildFlag("--cosa cosa","-c","desc!", func (value string){
+		//nothing
+	})
+	assertNil(t,err)
+	assertEqualsI(t,int(Option),int(flag.Type))
+	assertEquals(t,"--cosa",flag.Long)
+	assertEquals(t,"-c",flag.Short)
+}
+
+
+func TestGetFlagTypeOption(t *testing.T){
+	fType,err:=getFlagType("--option OPTION")
+	assertNil(t,err)
+	assertEqualsI(t,int(Option),int(fType))
+}
+
+func TestGetFlagTypeSwitch(t *testing.T){
+	fType,err:=getFlagType("--switch")
+	assertNil(t,err)
+	assertEqualsI(t,int(Switch),int(fType))
+}
+
+func TestGetFlagTypeEmpty(t *testing.T){
+	fType,err:=getFlagType("")
+	if (err==nil) {t.Error("No error thrown")}
+	assertEqualsI(t,-1,int(fType))
+}
+
+func TestGetFlagTypeTooLong(t *testing.T){
+	fType,err:=getFlagType("--cosa otra hahha")
+	if (err==nil) {t.Error("No error thrown")}
+	assertEqualsI(t,-1,int(fType))
+}
+
+func TestGetFlagNameOption(t *testing.T){
+	name,err:=getFlagLonfDefinition("--cosa THINGY")
+	assertNil(t,err)
+	assertEquals(t,"--cosa",name)
+}
+
+func TestGetFlagNameSwitch(t *testing.T){
+	name,err:=getFlagLonfDefinition("--cosa")
+	assertNil(t,err)
+	assertEquals(t,"--cosa",name)
+}
+func TestGetFlagNoPrefix(t *testing.T){
+	_,err:=getFlagLonfDefinition("-cosa")
+	if (err==nil) {t.Error("No error thrown")}
+}
+func TestGetFlagShortName(t *testing.T){
+	name,err:=getFlagShortDefinition("-c")
+	assertNil(t,err)
+	assertEquals(t,"-c",name)
+}
+
+func TestGetFlagShortNameTooManyWords(t *testing.T){
+	_,err:=getFlagShortDefinition("-c a")
+	if (err==nil) {t.Error("No error thrown")}
+}
+func TestGetFlagShortNameNoPrefix(t *testing.T){
+	_,err:=getFlagShortDefinition("#c")
+	if (err==nil) {t.Error("No error thrown")}
+}
+
+func TestAddCommand(t *testing.T){
+	parser:=NewParser("test")
+	command,err:=parser.AddCommand("com",emptyFn)
+	assertNil(t,err)
+	assertEquals(t,"com",command.Name)
+	if _,exists:=parser.Commands["com"];!exists{
+		t.Error("command not inserted")
+	}
+
+}
+
+func TestAddCommandTwice(t *testing.T){
+	parser:=NewParser("test")
+	_,err:=parser.AddCommand("com",emptyFn)
+	_,err=parser.AddCommand("com",emptyFn)
+	if (err==nil) {t.Error("No error thrown")}
+
+}
+func TestParseGlobalOption(t *testing.T){
+	parser:=NewParser("test")
+	processed:=false
+	parser.AddFlag("--option OPT","-o","This is an option",func(val string){
+		if val=="value"{
+			processed=true
+		}
+	})
+	parser.Parse([]string{"--option","value"})
+	if !processed {t.Error("Option wasn't processed")}
+
+}
+
+func TestParseGlobalOptionShort(t *testing.T){
+	parser:=NewParser("test")
+	processed:=false
+	parser.AddFlag("--option OPT","-o","This is an option",func(val string){
+		if val=="value"{
+			processed=true
+		}
+	})
+	parser.Parse([]string{"-o","value"})
+	if !processed {t.Error("Option wasn't processed")}
+
+}
+
+func TestParseGlobalSwitch(t *testing.T){
+	parser:=NewParser("test")
+	processed:=false
+	parser.AddFlag("--switch","-s","This is a switch",func(string){
+		processed=true
+	})
+	parser.Parse([]string{"--switch","value"})
+	if !processed {t.Error("Switch wasn't processed")}
+
+}
+
+func TestParseGlobalSwitchShort(t *testing.T){
+
+	parser:=NewParser("test")
+	processed:=false
+	parser.AddFlag("--switch","-s","This is a switch",func(string){
+		processed=true
+	})
+	parser.Parse([]string{"-s","value"})
+	if !processed {t.Error("Switch wasn't processed")}
+
+}
+
+func TestParseGlobalNoOptionFound(t *testing.T){
+	parser:=NewParser("test")
+	_,err:=parser.Parse([]string{"--nanana","value"})
+	if (err==nil) {t.Error("No error thrown")}
+}
+
+func TestParseGlobalOptionEmpty(t *testing.T){
+	parser:=NewParser("test")
+	parser.AddFlag("--option ","-o","This is an option",emptyFn)
+	_,err:=parser.Parse([]string{"--option"})
+	if (err==nil) {t.Error("No error thrown")}
+}
+
+func TestParseCommand(t *testing.T){
+	parser:=NewParser("test")
+	proc:=false
+	parser.AddCommand("command",func (string){
+		proc=true
+	})
+	parser.Parse([]string{"command"})
+	if !proc {t.Error("Command wasn't processed")}
+}
+
+func TestParseUnknown(t *testing.T){
+	parser:=NewParser("test")
+	parser.AddCommand("command",func (string){
+	})
+	leftOvers,_:=parser.Parse([]string{"paco","pepe"})
+	if(len(leftOvers)!=2){
+		t.Error("the parsing leftovers size isn't 2 (%v)",leftOvers)
+		return
+	}
+
+	if leftOvers[0]!="paco"  {
+		t.Error("First element  of the leftovers is wrong")
+		return
+	}
+	if (leftOvers[1]!="pepe" ) {
+		t.Error("Second element of the leftovers is wrong")
+		return
+	}
+}
+
+func TestParseInnerFlagCommand(t *testing.T){
+	parser:=NewParser("test")
+	shouldnt:=false
+	proc:=false
+	parser.AddFlag("--switch","-s","This is a global switch",func (string){
+		shouldnt=true
+	})
+	cmd,_:=parser.AddCommand("command",func (string){
+	})
+	cmd.AddFlag("--switch","-s","This is a command switch",func (string){
+		proc=true
+	})
+	parser.Parse([]string{"command","-s"})
+	if !proc {t.Error("Switch wasn't processed")}
+	if shouldnt{t.Error("Confusion between global and command flag")}
+}
+
